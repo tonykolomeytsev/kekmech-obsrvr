6a6d85889c4eeadd414c3911a6552ada8b003410
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2019-06-11 13:28:41 +0300

    Debugged the work of the snapshots generator

--- obsrvr.py
+++ obsrvr.py
@@ -1,36 +1,17 @@
-from picamera.array import PiRGBArray
-import picamera
-import cv2
-import numpy as np
-points = []
 
-def click(event, x, y, flags, param):
-	global points
-	if (len(points) == 4) and (event == cv2.EVENT_LBUTTONDOWN):
-		points = []
-		print(points)
-	if event == cv2.EVENT_LBUTTONDOWN:
-		points.append((x, y))
-		print(points)
-	if event == cv2.EVENT_RBUTTONDOWN:
-		points = []
-		print(points)
+import cv2
+import time
+import sys
+import argparse
+import os
+import obsrvrsnaps as o_snaps
+
+def calibrate_camera(resolution=(800,480)):
+	print("""Camera will now be calibrated. Follow the instructions on the screen.
+Take about 20 snapshots, then press `q`""")
+	input("Press any key to continue...\n")
+	w, h = resolution
+	o_snaps.save_snaps(width=w, height=h)
+	input("Snapshots saved. \nPress any key to generate transform matrix...")
 	
-def init_camera():
-	cv2.namedWindow("Frame", cv2.WINDOW_NORMAL)
-	cv2.setMouseCallback("Frame", click)
-	cv2.resizeWindow("Frame", 320, 240)
 	
-	with picamera.PiCamera() as camera:
-		camera.resolution =(640, 480)
-		camera.framerate = 32
-		rawCapture = PiRGBArray(camera, size=(640, 480))
-		for frame in camera.capture_continuous(rawCapture, format='bgr', use_video_port=True):
-			frame = frame.array
-			rawCapture.truncate(0)
-			cv2.imshow("Frame", frame)
-			if cv2.waitKey(1) & 0xFF == ord('q'):
-				break
-	cv2.destroyAllWindows()
-		##frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	return np.array(points)
--- obsrvrsnaps.py
+++ obsrvrsnaps.py
@@ -10,14 +10,16 @@ import os
 
 
 def save_snaps(width=0, height=0, name="snapshot", folder=".", raspi=False):
+    cv2.CAP_PROP_FRAME_WIDTH = 3
+    cv2.CAP_PROP_FRAME_HEIGHT = 4
     if raspi:
         os.system("sudo modprobe bcm2835-v412")
-    cap = cv2.VideoCaptore(0)
+    cap = cv2.VideoCapture(0)
 
     if width > 0 and height > 0:
-        print("Setting the custom width and height")
-        cap.set(cv2.CAP_PROB_FRAME_WIDTH, width)
-        cap.set(cv2.CAP_PROB_FRAME_HEIGHT, height)
+        print("Setting the custom width and height: {}x{}".format(width,height))
+        cap.set(3, width)
+        cap.set(4, height)
     try:
         if not os.path.exists(folder):
             os.makedirs(folder)
@@ -30,20 +32,23 @@ def save_snaps(width=0, height=0, name="snapshot", folder=".", raspi=False):
         pass
     
     nSnap   = 0
-    w       = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-    h       = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    w       = cap.get(3)
+    h       = cap.get(4)
 
-    fileName = "{}/{}_{}_{}_".format(folder, name, w, h)
+    fileName = "{}/{}_{}_{}_".format(folder, name, int(w), int(h))
+    print("Press Whitespace to make shapshot, press `q` when complete.")
     while True:
+        time.sleep(0.2)
         ret, frame = cap.read()
         cv2.imshow('camera', frame)
 
-        key = cv2.waitkey(1) & 0xFF
+        key = cv2.waitKey(1) & 0xFF
         if key == ord('q'):
             break
         if key == ord(' '):
-            print("saving image", nSnap)
-            cv2.imwrite("{}{}.jpg".format(fileName, nSnap), frame)
+            img_filename = "{}{}.jpg".format(fileName, nSnap)
+            print("Saving image {}".format(img_filename), nSnap)
+            cv2.imwrite(img_filename, frame)
             nSnap += 1
     
     cap.release()
